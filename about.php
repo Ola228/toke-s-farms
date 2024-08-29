d3b8d3d1d5e0684f90e233f321ecf1fd6f6aaeae
Ola228 <148010847+Ola228@users.noreply.github.com>
2024-08-29 17:51:18 +0100

    first commit

---
+++ about.php
@@ -0,0 +1,372 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<!-- Mirrored from validthemes.net/site-template/agrica/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Apr 2024 08:29:08 GMT -->
+
+<head>
+    <!-- ========== Meta Tags ========== -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Agrica - Organic Farm Agriculture Template">
+
+    <!-- ========== Page Title ========== -->
+    <title>Toke's Farms || About Us</title>
+
+    <!-- ========== Favicon Icon ========== -->
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+
+    <!-- ========== Start Stylesheet ========== -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="assets/css/magnific-popup.css" rel="stylesheet">
+    <link href="assets/css/flaticon-set.css" rel="stylesheet">
+    <link href="assets/css/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/css/animate.min.css" rel="stylesheet">
+    <link href="assets/css/validnavs.css" rel="stylesheet">
+    <link href="assets/css/helper.css" rel="stylesheet">
+    <link href="assets/css/unit-test.css" rel="stylesheet">
+    <link href="assets/css/shop.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- ========== End Stylesheet ========== -->
+
+    <!--[if lte IE 9]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Start Preloader 
+    ============================================= -->
+    <?php include 'nav.php' ?>
+    <!-- End Header -->
+
+    <!-- Start Breadcrumb 
+    ============================================= -->
+    <div class="breadcrumb-area text-center shadow dark-hard bg-cover text-light" style="background-image: url(assets/img/banner/15.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <h1>About Us</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li><a href="index-2.html"><i class="fas fa-home"></i> Home</a></li>
+                            <li class="active">About</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Breadcrumb -->
+
+    <!-- Start About 
+    ============================================= -->
+    <div class="about-style-one-area default-padding overflow-hidden">
+        <div class="container">
+            <div class="row align-center">
+                <div class="col-xl-6 col-lg-5">
+                    <div class="about-style-one-thumb">
+                        <img src="assets/img/about/1.jpg" alt="Image Not Found">
+                        <div class="animation-shape">
+                            <img src="assets/img/illustration/1.png" alt="Image Not Found">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-5 offset-xl-1 col-lg-6 offset-lg-1">
+                    <div class="about-style-one-info">
+
+                        <h2 class="title">Welcome to Toke's Agroenterprises</h2>
+                        <p>
+                            At Toke's Agroenterprises, we believe in the transformative power of aquaculture and sustainable agriculture to drive positive change in our community. Our mission is to empower women by providing them with the skills, resources, and opportunities needed to excel in the aquaculture industry and beyond.
+                        </p>
+                        <!-- <div class="fun-fact-style-flex mt-35">
+                            <div class="counter">
+                                <div class="timer" data-to="25" data-speed="2000">25</div>
+                                <div class="operator">M</div>
+                            </div>
+                            <span class="medium">Growth Tonns <br> of Harvest</span>
+                        </div> -->
+                        <ul class="top-feature">
+                            <li>
+                                <div class="icon">
+                                    <!-- <img src="assets/img/icon/3.png" alt="Image Not Found"> -->
+                                    <h3>Our Mission</h3>
+                                </div>
+                                <div class="info">
+                                    <!-- <h4>100% Guaranteed Organic Product</h4> -->
+                                    <p>
+                                        Our mission is to foster economic independence and create sustainable livelihoods for women through comprehensive training and support in aquaculture and agriculture. We are committed to promoting food security, environmental sustainability, and community well-being.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="icon">
+                                    <!-- <img src="assets/img/icon/2.png" alt="Image Not Found"> -->
+                                    <h3>Our Vission</h3>
+                                </div>
+                                <div class="info">
+                                    <!-- <h4>Top-Quality Healthy Foods Production</h4> -->
+                                    <p>
+                                        We envision a future where women are at the forefront of sustainable aquaculture and agricultural practices, leading their communities with innovation, resilience, and empowerment. Our goal is to be a beacon of change, inspiring women to realize their full potential and contribute to a thriving, self-sufficient community.
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End About -->
+
+    <!-- Start Timeline 
+    ============================================= -->
+    <div class="timeline-area default-padding-bottom" style="background-image: url(assets/img/shape/21.png);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <div class="site-heading">
+                        <h2 class="title">Farming have been <br> since 1866</h2>
+                        <div class="row">
+                            <div class="col-xl-10 offset-xl-2">
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam eaque laborum ad asperiores dolorem. Adipisci error eaque dolorem, itaque aliquam animi fuga dolor ipsam! Velit ratione hic corporis veritatis odit.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="timeline-items">
+                        <!-- Single Item -->
+                        <div class="timeline-item">
+                            <h2>1906</h2>
+                            <h4>Open Farm</h4>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="timeline-item">
+                            <h2>1920</h2>
+                            <h4>Farm Remodelacion</h4>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="timeline-item">
+                            <h2>1925</h2>
+                            <h4>Grainfarmers Formed</h4>
+                        </div>
+                        <!-- End Single Item -->
+                        <!-- Single Item -->
+                        <div class="timeline-item">
+                            <h2>1930</h2>
+                            <h4>Start of Agriculture</h4>
+                        </div>
+                        <!-- End Single Item -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Timeline -->
+
+    <!-- Start Choose Us 
+    ============================================= -->
+    <div class="choose-us-style-three-area default-padding bg-dark text-light">
+        <div class="illustration-bottom">
+            <img src="assets/img/illustration/2.png" alt="Image Not Found">
+        </div>
+        <div class="shape" style="background-image: url(assets/img/about/3.jpeg);"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-6 pl-60 pl-md-15 pl-xs-15">
+                    <h2 class="title">Our Approach</h2>
+                    <p>
+                        We offer a range of programs designed to meet the diverse needs of women in our community, including hands-on training in aquaculture techniques, sustainable agriculture practices, and business development workshops. Our approach is centered on practical learning, community support, and a commitment to creating lasting change.
+                    </p>
+                    <div class="list-grid">
+
+                        <div class="achivement-content">
+                            <div class="item">
+                                <div class="progressbar">
+                                    <div class="circle" data-percent="87">
+                                        <strong></strong>
+                                    </div>
+                                </div>
+                                <h4>Our Values</h4>
+                            </div>
+                        </div>
+                        <ul class="list-item">
+                            <li>Empowerment</li>
+                            <li>Sustainability</li>
+                            <li>Community</li>
+                            <li>Innovation</li>
+                            <li>Integrity</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Choose Us -->
+
+    <!-- Start Team 
+    ============================================= -->
+    <div class="team-style-one-area default-padding">
+        <div class="container">
+            <div class="row align-center">
+                <div class="col-lg-4">
+                    <h4 class="sub-title">Our Farmers</h4>
+                    <h2 class="title">Meet our professional farm experts</h2>
+                    <a class="btn btn-theme secondary mt-10 btn-md radius animation" href="farmers.html">Meet all Farmers</a>
+                </div>
+                <div class="col-lg-7 offset-lg-1">
+                    <div class="team-style-one-carousel swiper">
+                        <!-- Additional required wrapper -->
+                        <div class="swiper-wrapper">
+
+                            <!-- Single Item -->
+                            <div class="swiper-slide">
+                                <div class="farmer-style-one-item">
+                                    <div class="thumb">
+                                        <img src="assets/img/farmers/1.jpg" alt="Image Not Found">
+                                        <div class="social">
+                                            <i class="fas fa-share-alt"></i>
+                                            <ul>
+                                                <li class="facebook">
+                                                    <a href="#">
+                                                        <i class="fab fa-facebook-f"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="twitter">
+                                                    <a href="#">
+                                                        <i class="fab fa-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="linkedin">
+                                                    <a href="#">
+                                                        <i class="fab fa-linkedin-in"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="info">
+                                        <span>Farmer of tomatoes</span>
+                                        <h4><a href="farmer-details.html">Aleesha Brown</a></h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Item -->
+                            <!-- Single Item -->
+                            <div class="swiper-slide">
+                                <div class="farmer-style-one-item">
+                                    <div class="thumb">
+                                        <img src="assets/img/farmers/2.jpg" alt="Image Not Found">
+                                        <div class="social">
+                                            <i class="fas fa-share-alt"></i>
+                                            <ul>
+                                                <li class="facebook">
+                                                    <a href="#">
+                                                        <i class="fab fa-facebook-f"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="twitter">
+                                                    <a href="#">
+                                                        <i class="fab fa-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="linkedin">
+                                                    <a href="#">
+                                                        <i class="fab fa-linkedin-in"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="info">
+                                        <span>Farmer of cherry</span>
+                                        <h4><a href="farmer-details.html">Kevin Martin</a></h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Item -->
+                            <!-- Single Item -->
+                            <div class="swiper-slide">
+                                <div class="farmer-style-one-item">
+                                    <div class="thumb">
+                                        <img src="assets/img/farmers/3.jpg" alt="Image Not Found">
+                                        <div class="social">
+                                            <i class="fas fa-share-alt"></i>
+                                            <ul>
+                                                <li class="facebook">
+                                                    <a href="#">
+                                                        <i class="fab fa-facebook-f"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="twitter">
+                                                    <a href="#">
+                                                        <i class="fab fa-twitter"></i>
+                                                    </a>
+                                                </li>
+                                                <li class="linkedin">
+                                                    <a href="#">
+                                                        <i class="fab fa-linkedin-in"></i>
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                    <div class="info">
+                                        <span>Farmer of potato</span>
+                                        <h4><a href="farmer-details.html">Sarah Albert</a></h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Item -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End Team -->
+
+    <!-- Start Footer 
+    ============================================= -->
+    <?php include 'footer.php' ?>
+    <!-- End Footer -->
+
+    <!-- jQuery Frameworks
+    ============================================= -->
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/jquery.appear.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/progress-bar.min.js"></script>
+    <script src="assets/js/circle-progress.js"></script>
+    <script src="assets/js/isotope.pkgd.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="assets/js/magnific-popup.min.js"></script>
+    <script src="assets/js/count-to.js"></script>
+    <script src="assets/js/jquery.scrolla.min.js"></script>
+    <script src="assets/js/ScrollOnReveal.js"></script>
+    <script src="assets/js/YTPlayer.min.js"></script>
+    <script src="assets/js/validnavs.js"></script>
+    <script src="assets/js/gsap.js"></script>
+    <script src="assets/js/ScrollTrigger.min.js"></script>
+    <script src="assets/js/SplitText.min.js"></script>
+    <script src="assets/js/main.js"></script>
+
+</body>
+
+<!-- Mirrored from validthemes.net/site-template/agrica/about-us.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 Apr 2024 08:29:08 GMT -->
+
+</html>
